a0c3da3887e4295455ee706865e5609f3302f16e
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-01-19 13:49:43 -0500

    started .js for problem 4

---
+++ letters.js
@@ -0,0 +1,16 @@
+/* 
+ * Danilo Zekovic
+ * Chepter 2, Exersises problem #4
+ * Create an object that stores individual letters in an array and 
+ * has a function for displaying the letters as a single word.
+ */
+
+// create an object
+function lettersObj() {
+  this.dataStore = [];
+  this.add = add;
+}
+
+
+// store letters in it 
+// display letters as a single word
