071267d042e264f96662153d6cf4fcef05a5778a
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-01-22 13:40:35 -0500

    Finished product. App prints out 'Danilo' which are the letters of the array

--- letters.js
+++ letters.js
@@ -5,12 +5,37 @@
  * has a function for displaying the letters as a single word.
  */
 
-// create an object
+// create an object, constructor
 function lettersObj() {
   this.dataStore = [];
   this.add = add;
+  this.concat = concat;
+}
+// add letter to the array
+function add(letter) {
+  this.dataStore.push(letter);
+}
+// add letters toghether
+function addThem (newString, letter) {
+  return newString + letter;
+}
+// access array from the object and put letters from it into one word
+// return the word created 
+function concat () {
+  return this.dataStore.reduce(addThem);
 }
 
+// create objec word
+var word = new lettersObj();
 
-// store letters in it 
-// display letters as a single word
+// add letters to array
+word.add("D");
+word.add("a");
+word.add("n");
+word.add("i");
+word.add("l");
+word.add("o");
+
+// print the word made out of letters from array in 'word'
+var newWord = word.concat();
+console.log(newWord);
